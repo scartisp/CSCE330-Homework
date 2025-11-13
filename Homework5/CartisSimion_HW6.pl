1109e70e7ccf2bae8e31810ee3939b69f0424bbd
scartisp <simion.cartis1@gmail.com>
2025-11-13 13:58:53 -0500

    completed HW6

--- Homework5/CartisSimion_HW6.pl
+++ Homework5/CartisSimion_HW6.pl
@@ -1,3 +1,4 @@
+%Simion Cartis
 %Q1: list of exactly length 3
 exactly_3([_|[_,_]]).
 /*
@@ -32,7 +33,7 @@ true.
 at_most_3([]).
 at_most_3([_|[]]).
 at_most_3([_|[_]]).
-at_most_3([_ | [_,_]]).
+at_most_3([_|[_,_]]).
 /*
 ?- at_most_3([]).
 true.
@@ -47,5 +48,30 @@ true .
 true.
 
 ?- at_most_3([1,2,3,4]).
+false.
+*/
+
+%Q4: true if list X has an element in common with list Y 
+interesect(X,Y):- member(Z,X), member(Z,Y).
+/*
+?- interesect([1,2,3], [3,4,5]).
+true ;
+false.
+
+?- interesect([1,2,3], [6,4,5]).
+false.
+*/
+
+%Q5: true if every list in list 1 has an element that is also in list Y 
+all_interesect([X|L],Y):- interesect(X,Y), all_interesect(L,Y).
+all_interesect([],_). 
+/*
+?- all_interesect([[1,2,3],[5,4,6]],[3,4]).
+true .
+
+?- all_interesect([],[3,4]).
+true.
+
+?- all_interesect([[1,2,3],[1,2,5],[5,4,6]],[3,4]).
 false.
 */
